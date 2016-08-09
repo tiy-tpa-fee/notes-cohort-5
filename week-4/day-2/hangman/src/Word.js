127b020cda408d8a4dd9349a6de526982cc46b3a
Jason L Perry <jasper@ambethia.com>
2016-08-09 15:45:52 -0400

    Update Word.js

--- week-4/day-2/hangman/src/Word.js
+++ week-4/day-2/hangman/src/Word.js
@@ -5,6 +5,9 @@ import './Word.css'
 class Word extends Component {
 
   render() {
+    // This is the kicker right here. We're going to split the string into an array
+    // of each letters, and `map` over that, making a <Letter /> compenent for each.
+    // Then we can put that right into our <div> in the output.
     const letters = this.props.solution.split('').map((letter, index) => {
       // Search the letters that have been "used" so far and see if
       // any of them are this letter, if it's undefeind, the letter is
